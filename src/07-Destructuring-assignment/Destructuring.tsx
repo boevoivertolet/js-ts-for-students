dd28a3f54421ff33b4e67e53e581f11b2c6c465e
boevoivertolet <bjha78rus@yandex.ru>
2022-05-05 16:00:21 +0300

    React.FC<PropsType> = ({title,man, ...props}) => {// Достань из проспсов тайтл и мэн. А всё остальное запихни в объект пропс

--- src/07-Destructuring-assignment/Destructuring.tsx
+++ src/07-Destructuring-assignment/Destructuring.tsx
@@ -14,20 +14,24 @@ export type ManType = {
 type PropsType = {
     title: string
     man: ManType
+    food: Array<string>
+    car:{model:string}
 
 }
 
-export const ManComponent: React.FC<PropsType> = (props) => {
+export const ManComponent: React.FC<PropsType> = ({title,man, ...props}) => {// Достань из проспсов тайтл и мэн. А всё остальное запихни в объект пропс
 
-   /* const {title} = props
-    const {name} = props.man*/
-    const{title, man:{name}} =props
+    /* const {title} = props
+     const {name} = props.man*/
+    /*const {title, man: {name}} = props*/
 
     return (
         <div>
             <h1>{title}</h1>
             <hr/>
-            <div>{name}</div>
+            <div>{man.name}</div>
+            <hr/>
+            <div>{props.car.model}</div>
         </div>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {User} from './06-callback-onClick-onChange/06';
 
