8f86214815e5820ae7187bd3f941ee9ce1e108bd
boevoivertolet <bjha78rus@yandex.ru>
2022-05-05 15:44:25 +0300

    /* const {title} = props     const {name} = props.man*/     ЭТО тоже самое, что и
        const{title, man:{name}} =props        ЭТО

--- src/07-Destructuring-assignment/destructuring.test.ts
+++ src/07-Destructuring-assignment/destructuring.test.ts
@@ -1,20 +1,38 @@
-test('', () => {
-    let props = {
+import {ManType} from './Destructuring';
+
+
+let props: ManType
+beforeEach(() => {
+    props = {
         name: 'Sasha',
         age: 34,
-        lessons: [{title: '1'}, {title: '2'}]
+        lessons: [{title: '1'}, {title: '2'}],
+        address: {
+            street: {
+                title: 'Nezavisimosti'
+            }
+        }
     }
-   /* const age = props.age
-    const lessons = props.lessons*/ //Эти 2 строки эквивалентны
+})
+
+
+test('', () => {
+
+    /* const age = props.age
+     const lessons = props.lessons*/ //Эти 2 строки эквивалентны
     const {age, lessons} = props // вот этим
+    const {title} = props.address.street
 
-
-    const a = props.age
-    const l = props.lessons
+/*    const a = props.age
+    const l = props.lessons*/
 
     expect(age).toBe(34)
     expect(lessons.length).toBe(2)
 
-    expect(a).toBe(34)
-    expect(l.length).toBe(2)
+
+    expect(title).toBe('Nezavisimosti')
+
+})
+test('', ()=> {
+
 })
