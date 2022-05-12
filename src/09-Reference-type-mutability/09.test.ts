183241f3fb6117bf2da18d915fd624eac94688c3
boevoivertolet <bjha78rus@yandex.ru>
2022-05-12 16:51:30 +0300

    create test 09

--- src/09-Reference-type-mutability/09.test.ts
+++ src/09-Reference-type-mutability/09.test.ts
@@ -1,23 +1,109 @@
 
-
-
-function increseAge(u:UserType) {
+function increseAge(u: UserType) {
     u.age++;
 
 }
-type UserType ={
+
+type AddressType = {
+    title: string
+}
+type UserType = {
     name: string
     age: number
+    address: AddressType
 }
 
-test('big test', ()=> {
-    let user: UserType ={
+test(' reference type test', () => {
+    let user: UserType = {
         name: 'Sasha',
-        age: 34
+        age: 34,
+        address: {
+            title: 'Minsk'
+        }
     }
 
     increseAge(user)
 
 
     expect(user.age).toBe(35)
+
+    const superMan = user
+    superMan.age = 1000;
+
+    expect(user.age).toBe(1000)
+
+})
+test('array test', () => {
+    let users = [
+        {
+            name: 'Sasha',
+            age: 34
+        },
+        {
+            name: 'Ulya',
+            age: 32
+        }
+
+    ]
+
+    let admins = users
+    admins.push({name: 'Taras', age: 21})
+
+    expect(users[2]).toEqual({name: 'Taras', age: 21})
+
+})
+test('array reference test', () => {
+    let usersCount = 100
+    let adminsCount = usersCount
+    adminsCount++
+
+    expect(adminsCount).toBe(101)
+
+})
+test('big reference type test', () => {
+    const address = {title: 'Minsk'}
+
+    let user: UserType = {
+        name: 'Sasha',
+        age: 34,
+        address: address
+    }
+
+    let addr = user.address
+    let user2: UserType = {
+        name: 'Nataha',
+        age: 28,
+        address: address
+    }
+    address.title = 'Minsk City'
+
+
+    expect(user.address.title).toBe('Minsk City')
+    expect(user.address).toBe(user2.address)
+
+})
+test('Array reference type test', () => {
+    const address = {title: 'Minsk'}
+
+    let user: UserType = {
+        name: 'Sasha',
+        age: 34,
+        address: address
+    }
+    let user2: UserType = {
+        name: 'Nataha',
+        age: 28,
+        address: address
+    }
+
+    const users = [user, user2, {name: 'Misha', age: 33, address: address}]
+
+    const admins = [user, user2]
+
+    admins[0].name = 'Dmitry'
+
+
+    expect(users[0].name).toBe('Dmitry')
+    expect(user.name).toBe('Dmitry')
+
 })
